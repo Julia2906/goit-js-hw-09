aea66f226ea17dacdfe7392e3b53649446084b69
Julia2906 <137899526+Julia2906@users.noreply.github.com>
2025-01-26 15:42:48 +0200

    without css task 2

---
+++ src/js/2-form.js
@@ -0,0 +1,58 @@
+const feedbackFormEl = document.querySelector('.feedback-form');
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+const fillFormFields = () => {
+  try {
+    if (localStorage.length === 0) {
+      return;
+    }
+
+    const formDataFromLS = JSON.parse(
+      localStorage.getItem('feedback-form-state')
+    );
+
+    formData = formDataFromLS;
+
+    for (const key in formDataFromLS) {
+      feedbackFormEl.elements[key].value = formDataFromLS[key];
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fillFormFields();
+
+const onFormFieldChange = event => {
+  const { target: formFieldEl } = event;
+  const fieldValue = formFieldEl.value.trim();
+  const fieldName = formFieldEl.name;
+
+  formData[fieldName] = fieldValue;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+feedbackFormEl.addEventListener('input', onFormFieldChange);
+
+const onFeedbackFormSubmit = event => {
+  event.preventDefault();
+
+  const { currentTarget: formEl } = event;
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  formEl.reset();
+  localStorage.removeItem('feedback-form-state');
+};
+
+feedbackFormEl.addEventListener('submit', onFeedbackFormSubmit);
